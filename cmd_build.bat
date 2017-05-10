95838ad1b72a3991c1c3be7d4e2dc5d59575a27e
Jakob Reiss Axelsen <jra@mv-nordic.com>
2017-05-10 10:09:35 +0100

    lol

---
+++ cmd_build.bat
@@ -0,0 +1,6 @@
+"C:\Qt\5.4\msvc2010_opengl\bin\qmake.exe" "C:\jenkins\jobs\Hackathon1c\workspace\Hack2.pro" -spec win32-msvc2010
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+"C:\Qt\Tools\QtCreator\bin\jom.exe" qmake_all
+"C:\Qt\Tools\QtCreator\bin\jom.exe"
+"C:\Qt\5.4\msvc2010_opengl\bin\windeployqt.exe" "C:\jenkins\jobs\Hackathon1c\workspace\release\Hack2.exe"
